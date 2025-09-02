b364c65ad87c4fa89a238bd0dd4d7bebd6662e41
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-09-02 16:36:16 +0200

    inizio con la biografia

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,12 @@ function App() {
           <h1 className="d-block mx-auto text-center"><strong>Actresses</strong></h1>
         </div>
         <div className="col-12">
-          <div className="card text-white">
+          <div className="card">
             <div className="card-header">Nome</div>
             <img src="" alt="attrici" />
             <div className="card-body text-center">
               <p>1099 &nbsp; americano</p>
+              <p className="card-text">Meryl Streep is considered one of the greatest actresses of her generation and has received numerous accolades for her versatile performances.</p>
             </div>
           </div>
         </div>
